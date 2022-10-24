fb49d59ed342c3695595de8598596f737fb69c67
giribabu22 <giribabu22@navgurukul.org>
2022-10-24 23:53:44 +0530

    ver 00.2.0

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,7 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize('uttarayan', "uttarayan", 'uttarayan', {
+module.exports = new Sequelize('uttarayan_app', "uttarayan_app", 'uttarayan_app', {
         host: "db4free.net",
         port: 3306,
         dialect: 'mysql',
-        logging: false
 })
